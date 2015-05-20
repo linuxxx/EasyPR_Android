296e04719b611cfb09ab89c9911be5b1c710b2c0
LL <1750299013@qq.com>
2015-05-20 13:02:34 +0800

    更改必须加载openmanger & 修改编辑字符集为UTF-8

--- jni/Android.mk
+++ jni/Android.mk
@@ -1,6 +1,9 @@
 LOCAL_PATH := $(call my-dir)/src
-include $(CLEAR_VARS)  
-include D:/OpenCV-2.4.10-android-sdk/OpenCV-2.4.10-android-sdk/sdk/native/jni/OpenCV.mk  
+include $(CLEAR_VARS)
+# OpenCV
+OPENCV_CAMERA_MODULES:=on
+OPENCV_INSTALL_MODULES:=on
+include /Users/LL/Desktop/ANDROID_SPACE/sdk/native/jni/OpenCV.mk  
 FILE_LIST := $(wildcard $(LOCAL_PATH)/core/*.cpp)
 LOCAL_SRC_FILES := $(FILE_LIST:$(LOCAL_PATH)/%=%)
 LOCAL_C_INCLUDES += $(LOCAL_PATH)/include
